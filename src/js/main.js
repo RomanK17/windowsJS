fd9a894b6c0bcc6b222c9aba4c88398ba8b21899
Roman Kopylov <romankopilov1712@gmail.com>
2023-04-17 11:40:06 +0300

    changes to comments
    changes:
    replace function creatingTabs arguments to object with properties;
    create isValidTabsTarget for expression in if condition;
    add optional chaining in isValidTabsTarget.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,21 @@
 import './slider';
 import modals from './modules/modals';
-import pageTabs from './modules/tabs';
+import creatingTabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
-    pageTabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-    pageTabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
+    creatingTabs(
+        {
+            headerSelector: '.glazing_slider',
+            tabsSelector: '.glazing_block',
+            contentSelector: '.glazing_content',
+            activeClass: 'active'
+        });
+    creatingTabs(
+        {
+            headerSelector: '.decoration_slider',
+            tabsSelector: '.no_click',
+            contentSelector: '.decoration_content > div > div',
+            activeClass: 'after_click'
+        });
 });
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,4 +1,4 @@
-const pageTabs = (headerSelector, tabsSelector, contentSelector, activeClass) => {
+const creatingTabs = ({ headerSelector, tabsSelector, contentSelector, activeClass }) => {
     const header = document.querySelector(headerSelector);
     const tabs = document.querySelectorAll(tabsSelector);
     const tabContent = document.querySelectorAll(contentSelector);
@@ -20,23 +20,20 @@ const pageTabs = (headerSelector, tabsSelector, contentSelector, activeClass) =>
 
     hideTabContent();
     showTabContent();
-    // можешь пояснить, зачем нужно заранее вызывать эти две функции? в видео особо не объясняется, я бы их убрал.
 
     header.addEventListener('click', (event) => {
         const target = event.target;
-        if (target) {
-            if (target.classList.contains(tabsSelector.replace(/\./, '')) ||
-                target.parentNode.classList.contains(tabsSelector.replace(/\./, ''))) {
-
-                tabs.forEach((tab, tabNumber) => {
-                    if (target == tab || target.parentNode == tab) {
-                        hideTabContent();
-                        showTabContent(tabNumber);
-                    }
-                });
-            }
+        const isValidTabsTarget = target?.parentNode.classList.contains(tabsSelector.replace(/\./, '')) || target?.classList.contains(tabsSelector.replace(/\./, ''));
+        if (isValidTabsTarget) {
+            console.log(target);
+            tabs.forEach((tab, tabNumber) => {
+                if (target == tab || target.parentNode == tab) {
+                    hideTabContent();
+                    showTabContent(tabNumber);
+                }
+            });
         }
     });
 };
 
-export default pageTabs;
+export default creatingTabs;
