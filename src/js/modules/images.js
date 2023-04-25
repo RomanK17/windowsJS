4ee6dbd4e5ecd67b731eae012c5d6d88c75e0cd4
Roman Kopylov <romankopilov1712@gmail.com>
2023-04-25 13:34:55 +0300

    add navigation on images

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -1,9 +1,10 @@
 const openImageModal = () => {
     const imagesContainer = document.querySelector('.works');
-
-    //creating modal for big image
+    const allImages = imagesContainer.querySelectorAll('.preview');
     const modalImage = document.createElement('div');
     const bigImage = document.createElement('img');
+
+    //creating modal for big image
     modalImage.classList.add('popup');
     imagesContainer.appendChild(modalImage);
     modalImage.style.justifyContent = 'center';
@@ -12,23 +13,43 @@ const openImageModal = () => {
     modalImage.appendChild(bigImage);
     bigImage.classList.add('bigImg');
 
-    imagesContainer.addEventListener('click', (event) => {
-        event.preventDefault();
-        const target = event.target;
+    // set tabindex for images and links
+    allImages.forEach((item) => {
+        item.setAttribute('tabindex', 0);
 
-        if (target && target.classList.contains('preview')) {
-            modalImage.style.display = 'flex';
-            document.body.style.overflow = 'hidden';
-
-            const pathBigImg = target.parentNode.getAttribute('href');
-            bigImage.setAttribute('src', pathBigImg);
+        const link = item.parentNode;
+        if (link && link.tagName === 'A') {
+            link.setAttribute('tabindex', -1);
         }
     });
+
+    const showModalImage = (event) => {
+        modalImage.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+
+        const pathBigImg = event.target.parentNode.getAttribute('href');
+        bigImage.setAttribute('src', pathBigImg);
+    };
 
     const closeModalImage = () => {
         modalImage.style.display = 'none';
         document.body.style.overflow = 'visible';
     };
+
+    imagesContainer.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target.closest('.preview');
+
+        if (target) {
+            showModalImage(event);
+        }
+    });
+
+    imagesContainer.addEventListener('keydown', (event) => {
+        if (event.code === 'Enter' && event.target.classList.contains('preview')) {
+            showModalImage(event);
+        }
+    });
 
     modalImage.addEventListener('click', closeModalImage);
 
