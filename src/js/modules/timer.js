1618eb326114b11b6464a28f79198ff30229b031
Roman Kopylov <romankopilov1712@gmail.com>
2023-04-25 11:29:59 +0300

    used method with toLocaleTimeString

--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -1,48 +1,25 @@
 const createTimer = () => {
-    const deadline = new Date(Date.now() + 10000000);
+    const deadline = new Date('2023-12-31T23:59:59');
 
-    const getRemaininngTime = (endTime) => {
-        const remainingTime = Date.parse(endTime) - Date.parse(new Date);
-        const days = Math.floor(remainingTime / 86400000);
-        const hours = Math.floor((remainingTime / 3600000) % 24);
-        const minutes = Math.floor((remainingTime / 60000) % 60);
-        const seconds = Math.floor((remainingTime / 1000) % 60);
+    const daysElement = document.querySelector('#days');
+    const hoursElement = document.querySelector('#hours');
+    const minutesElement = document.querySelector('#minutes');
+    const secondsElement = document.querySelector('#seconds');
 
-        return {
-            remainingTime,
-            days,
-            hours,
-            minutes,
-            seconds
-        };
+    const setTimer = () => {
+        const remainingTime = new Date(deadline - Date.now());
+        const daysLeft = Math.floor(remainingTime / 86400000);
+        const [hoursLeft, minutesLeft, secondsLeft] = remainingTime.toLocaleTimeString('ru-Ru', { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' }).split(':');
+
+        daysElement.innerHTML = daysLeft;
+        hoursElement.innerHTML = hoursLeft;
+        minutesElement.innerHTML = minutesLeft;
+        secondsElement.innerHTML = secondsLeft;
     };
-
-    const checkForZero = (num) => {
-        return num >= 0 && num < 10 ? `0${num}` : num;
-    }
-
-    const setTimer = (endTime) => {
-        const daysElement = document.querySelector('#days');
-        const hoursElement = document.querySelector('#hours');
-        const minutesElement = document.querySelector('#minutes');
-        const secondsElement = document.querySelector('#seconds');
-
-
-
-        const initializeTimer = () => {
-            const time = getRemaininngTime(endTime);
-            daysElement.innerHTML = checkForZero(time.days);
-            hoursElement.innerHTML = checkForZero(time.hours);
-            minutesElement.innerHTML = checkForZero(time.minutes);
-            secondsElement.innerHTML = checkForZero(time.seconds);
-        };
-
-        initializeTimer();
-        setInterval(initializeTimer, 1000);
-
-    };
-
     setTimer(deadline);
+
+    setInterval(setTimer, 1000);
+
 };
 
 export default createTimer;
